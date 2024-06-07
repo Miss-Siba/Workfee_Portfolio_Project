fb1b87d94f1a7d7a8e2e20285c7f738d9beed383
Siba <sibaquma@gmail.com>
2024-06-07 14:10:29 -0700

    requirements

--- setup_mysql_dev.sql
+++
@@ -1,5 +0,0 @@
--- MySQL Test server for the project.
-CREATE DATABASE IF NOT EXISTS workfee_db;
-CREATE USER IF NOT EXISTS 'workfee_dev'@'localhost' IDENTIFIED BY 'workfee_dev_pwd';
-GRANT ALL PRIVILEGES ON workfee_dev_db . * TO 'workfee_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'workfee_dev'@'localhost';
